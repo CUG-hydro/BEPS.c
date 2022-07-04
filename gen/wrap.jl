66123cb3b468e0fc0833b805c0889cb3ad20ff6d
Dongdong Kong <kongdd@users.noreply.github.com>
2022-07-04 12:35:54 +0800

    add julia wraper

--- gen/wrap.jl
+++ gen/wrap.jl
@@ -4,7 +4,7 @@ using Clang.Generators
 headers = ["../include/soil.h", "../include/beps.h"]
 args = get_default_args()
 
-
+options = load_options(joinpath(@__DIR__, "generator.toml"))
 
 ctx = create_context(headers, args)
 build!(ctx)
